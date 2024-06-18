426d2a83adf22e5f7727f51fdcb9f6b780cbfc05
Gabriels <gabripub@gmail.com>
2024-06-18 22:44:28 +0300

    ep: Add Management/Gameserver Endpoints

--- src/Paynow/Endpoint/ManagementEndpoints.ts
+++ src/Paynow/Endpoint/ManagementEndpoints.ts
@@ -1,14 +1,16 @@
-import { CustomerEndpoints, StoreEndpoints } from ".";
+import { CustomerEndpoints, GameserverEndpoints, StoreEndpoints } from ".";
 import { BaseEndpoint } from "../../lib";
 
 export class ManagementEndpoints extends BaseEndpoint {
   private readonly __stores: StoreEndpoints;
   private readonly __customers: CustomerEndpoints;
+  private readonly __gameservers: GameserverEndpoints;
 
   constructor(store_id: string) {
     super(store_id);
     this.__stores = new StoreEndpoints(this._storeId);
     this.__customers = new CustomerEndpoints(this._storeId);
+    this.__gameservers = new GameserverEndpoints(this._storeId);
   }
 
   /** Store endpoints */
@@ -19,5 +21,10 @@ export class ManagementEndpoints extends BaseEndpoint {
   /** Customer endpoints */
   public get customers(): CustomerEndpoints {
     return this.__customers;
+  }
+
+  /** Gameserver endpoints */
+  public get gameservers(): GameserverEndpoints {
+    return this.__gameservers;
   }
 }
--- src/Paynow/Endpoint/index.ts
+++ src/Paynow/Endpoint/index.ts
@@ -1,3 +1,4 @@
 export * from "./Management/Customer";
+export * from "./Management/Gameserver";
 export * from "./Management/Store";
 export * from "./ManagementEndpoints";
