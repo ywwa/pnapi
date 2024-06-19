f78d4ee88323cfb5a6a9dd6ac51f3d119f468cde
Gabriels <gabripub@gmail.com>
2024-06-19 22:01:05 +0300

    ep: Add Management/Sale Endpoints
    ep: Update exports

---
+++ src/Paynow/Endpoint/Management/Sale/Endpoints.ts
@@ -0,0 +1,16 @@
+import { BaseEndpoint } from "../../../../lib";
+
+export class SaledEndpoints extends BaseEndpoint {
+  constructor(store_id: string) {
+    super(store_id);
+  }
+
+  public base(): string {
+    return this._buildUrl(this._baseUrl, "stores", this._storeId, "sales");
+  }
+
+  public by_id(sale_id: string): string {
+    this._validate({ sale_id });
+    return this._buildUrl(this.base(), sale_id);
+  }
+}
