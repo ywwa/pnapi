fca2aae9ebfc07ce090bff41327193904cf2ed74
Gabriels <gabripub@gmail.com>
2024-06-19 17:31:25 +0300

    ep: Add Management/Subscription Endpoints

--- src/Paynow/Endpoint/index.ts
+++ src/Paynow/Endpoint/index.ts
@@ -4,5 +4,6 @@ export * from "./Management/Navlink";
 export * from "./Management/Order";
 export * from "./Management/Product";
 export * from "./Management/Store";
+export * from "./Management/Subscription";
 export * from "./Management/Tag";
 export * from "./ManagementEndpoints";
